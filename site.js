cee79d4967770d6e8a2e6eeef942b0b26b36bdce
223jmuirhead <84456296+223jmuirhead@users.noreply.github.com>
2023-04-04 11:50:12 -0400

    Buttons now function

--- site.js
+++ site.js
@@ -13,7 +13,75 @@ const vue_app = Vue.createApp({
         }
     },
     methods: {
+      toggleA: function() {
+        this.schedules[0].isActive = true;
+        this.schedules[1].isActive = true;
+        this.schedules[2].isActive = true;
+        this.schedules[3].isActive = false;
+        this.schedules[4].isActive = true;
+        this.schedules[5].isActive = true;
+        this.schedules[6].isActive = false;
+      },
+      toggleB: function() {
+        this.schedules[0].isActive = true;
+        this.schedules[1].isActive = true;
+        this.schedules[2].isActive = false;
+        this.schedules[3].isActive = true;
+        this.schedules[4].isActive = true;
+        this.schedules[5].isActive = false;
+        this.schedules[6].isActive = true;
+      },
+      toggleC: function() {
+        this.schedules[0].isActive = true;
+        this.schedules[1].isActive = false;
+        this.schedules[2].isActive = true;
+        this.schedules[3].isActive = true;
+        this.schedules[4].isActive = false;
+        this.schedules[5].isActive = true;
+        this.schedules[6].isActive = true;
+      },
+      toggleD: function() {
+        this.schedules[0].isActive = false;
+        this.schedules[1].isActive = true;
+        this.schedules[2].isActive = true;
+        this.schedules[3].isActive = true;
+        this.schedules[4].isActive = true;
+        this.schedules[5].isActive = true;
+        this.schedules[6].isActive = false;
+      },
+      toggleE: function() {
+        this.schedules[0].isActive = true;
+        this.schedules[1].isActive = true;
+        this.schedules[2].isActive = true;
+        this.schedules[3].isActive = false;
+        this.schedules[4].isActive = true;
+        this.schedules[5].isActive = false;
+        this.schedules[6].isActive = true;
+      },
+      toggleF: function() {
+        this.schedules[0].isActive = true;
+        this.schedules[1].isActive = true;
+        this.schedules[2].isActive = true;
+        this.schedules[3].isActive = false;
+        this.schedules[4].isActive = true;
+        this.schedules[5].isActive = false;
+        this.schedules[6].isActive = true;
+      },
+      toggleG: function() {
+        this.schedules[0].isActive = false;
+        this.schedules[1].isActive = false;
+        this.schedules[2].isActive = true;
+        this.schedules[3].isActive = true;
+        this.schedules[4].isActive = true;
+        this.schedules[5].isActive = true;
+        this.schedules[6].isActive = true;
+      }
 
+    },
+    computed: {
+      filteredS() {
+        return this.schedules.filter((schedule) => schedule.isActive)
+      }
     }
 })
 
